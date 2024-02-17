c3f07e0368ba4bcdd95599a67a08bc9f0b49b948
Mako Mestumrishvili <123207308+mkkusa@users.noreply.github.com>
2024-02-17 23:34:32 +0400

    Add files via upload

---
+++ Learning/lession1/main.js
@@ -0,0 +1,7 @@
+
+let socialMedia = ['Instagram', 'Facebook', 'Twitter'];
+
+for (let socialMediaIndex = 0; socialMediaIndex < socialMedia.length; socialMediaIndex++) {
+    console.log(socialMedia[socialMediaIndex]);
+}
+
